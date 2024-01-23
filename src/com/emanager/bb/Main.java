0c861fb2938ff7482f4cd3608f31cac6f3bd25ff
Bálint <balazs.balint5@gmail.com>
2024-01-23 23:47:42 +0100

    Programmer class add/remove from programming languange and team colleagues implemented

--- src/com/emanager/bb/Main.java
+++ src/com/emanager/bb/Main.java
@@ -1,10 +1,9 @@
 package com.emanager.bb;
 
-import com.emanager.bb.models.Gender;
-import com.emanager.bb.models.Manager;
-import com.emanager.bb.models.Meeting;
+import com.emanager.bb.models.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 
 public class Main {
@@ -13,15 +12,29 @@ public class Main {
         Manager man1 = new Manager("Manager", 45, "063034349", "manager1email",
                 LocalDate.now(), 10000.0, Gender.MALE);
 
-        Meeting meet1 = new Meeting("Morning meeting", LocalDate.now());
-        Meeting meet2 = new Meeting("Night meeting", LocalDate.now());
+        Programmer pr1 = new Programmer("Programmer", 28, "042321321",
+                "programmer@email", LocalDate.now(), 50000.0, Gender.FEMALE, "post-it");
+        Programmer pr2 = new Programmer("Jack", 28, "042321321",
+                "programmer@email", LocalDate.now(), 50000.0, Gender.FEMALE, "post-it");
 
-        meet1.addAttendee(man1);
+        man1.addEmployee(pr1);
+        pr1.addProgrammingLanguage(ProgramLanguages.JAVA);
+        HashSet<ProgramLanguages> hashSetPr = pr1.getProgramLanguages();
+        hashSetPr.add(ProgramLanguages.ANGULAR);
+        pr1.addProgrammingLanguage(ProgramLanguages.PHP);
+        pr1.addProgrammingLanguage(null);
 
-        man1.addMeeting(null);
-        man1.addMeeting(meet1);
-        man1.removeMeeting(meet2);
+        System.out.println(pr1.getProgramLanguages());
+        System.out.println(hashSetPr);
+//        Meeting meet1 = new Meeting("Morning meeting", LocalDate.now());
+//        Meeting meet2 = new Meeting("Night meeting", LocalDate.now());
+//
+//        meet1.addAttendee(man1);
+//
+//        man1.addMeeting(null);
+//        man1.addMeeting(meet1);
+//        man1.removeMeeting(meet2);
 
-        System.out.println(meet1);
+
     }
 }
--- src/com/emanager/bb/models/Programmer.java
+++ src/com/emanager/bb/models/Programmer.java
@@ -1,40 +1,58 @@
 package com.emanager.bb.models;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
 
 public class Programmer extends BaseEmployee{
 
     private String activeProject;
-    private List<ProgramLanguages> programLanguages;
-    private List<Programmer> teamColleagues;
+    private HashSet<ProgramLanguages> programLanguages;
+    private HashSet<Programmer> teamColleagues;
 
-    public Programmer(int age, String name, String telephone, String email, LocalDate startedAt, double salary, Gender gender, String activeProject, List<ProgramLanguages> programLanguages, List<Programmer> teamColleagues) {
+    public Programmer(String name, int age, String telephone, String email, LocalDate startedAt, double salary, Gender gender, String activeProject) {
         super(name, age, telephone, email, startedAt, salary, gender);
         this.activeProject = activeProject;
-        this.programLanguages = new ArrayList<>();
-        this.teamColleagues = teamColleagues;
+        this.programLanguages = new HashSet<>();
+        this.teamColleagues = new HashSet<>();
     }
 
     public String getActiveProject() {
         return activeProject;
     }
 
-    public List<ProgramLanguages> getProgramLanguages() {
-        return programLanguages;
+    public HashSet<ProgramLanguages> getProgramLanguages() {
+        return new HashSet<>(programLanguages);
     }
 
-    public List<Programmer> getTeamColleagues() {
-        return teamColleagues;
+    public void addProgrammingLanguage(ProgramLanguages programLanguage) {
+
+        if(programLanguage != null){
+            programLanguages.add(programLanguage);
+        }
+    }
+
+    public void removeProgrammingLanguage(ProgramLanguages programLanguage){
+
+        try{
+            programLanguages.remove(programLanguage);
+        } catch (NoSuchElementException e) {
+            System.out.println("There is no such programming language in the list like: " + programLanguage);
+        } catch (NullPointerException e) {
+            System.out.println("The programming language can't be null! Try again with different data!");
+        }
+    }
+
+    public HashSet<Programmer> getTeamColleagues() {
+        return new HashSet<>(teamColleagues);
     }
 
     @Override
     public String toString() {
-        return "Programmer{" +
-                "activeProject='" + activeProject + '\'' +
-                ", programLanguages=" + programLanguages +
-                ", teamColleagues=" + teamColleagues +
-                '}';
+        return super.toString() + " Programmer{ " +
+                "activeProject= '" + activeProject + '\'' +
+                ", programLanguages= " + programLanguages +
+                ", teamColleagues= " + teamColleagues +
+                " }";
     }
 }
